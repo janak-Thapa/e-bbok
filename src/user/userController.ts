8f873a892c1f2d4c977c18debb49c1be8a60ef48
janak-Thapa <ogjanak1@gmail.com>
2024-05-18 16:30:21 +0545

    add login endpoint

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -65,4 +65,36 @@ const createUser = async(req:Request,res:Response,next:NextFunction)=>{
 }
 
 
-export {createUser};
+
+const loginUser = async(req:Request,res:Response,next:NextFunction)=>{
+
+  const {email,password} = req.body;
+  if(!email || !password){
+    return next(createHttpError(400,"All fields are required"));
+
+  }
+
+  
+    const user = await userModel.findOne({email})
+    if(!user){
+      
+      return next(createHttpError(404,"User not found"));
+    }
+
+  
+  const isMatch = await bcrypt.compare(password,user.password as string)
+  if(!isMatch){
+    return next(createHttpError(400,"Username or password incorrect"));
+
+  }
+
+  const token = sign({sub:user},config.jwtSecret as string,{
+    expiresIn:"7d"
+  })
+  res.json({accessToken:token})
+}
+
+
+
+
+export {createUser,loginUser};
--- src/user/userRouter.ts
+++ src/user/userRouter.ts
@@ -1,11 +1,14 @@
 import  express  from "express";
 import { createUser } from "./userController";
+import {loginUser} from "./userController"
 
 
 const userRouter = express.Router();
 
 
 userRouter.post("/register",createUser)
+
+userRouter.post("/login",loginUser)
 
 
 export default userRouter;
