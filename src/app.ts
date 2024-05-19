f4f6da8d8357ebf4df659eab9cded10f6deb32d2
janak-Thapa <ogjanak1@gmail.com>
2024-05-19 16:15:53 +0545

    add cors support

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
 import express from "express";
+import cors from "cors";
 import golbalErrorHnadler from "./middlewares/globalErrorHndler";
 import userRouter from "./user/userRouter";
 import bookRouter from "./book/bookRouter";
+import { config } from "./config/config";
 
 
 const app = express();
+
+app.use(cors({
+  origin:config.frontendDomain,
+}))
 
 app.use(express.json())
 
